dcd56f3e4250d2fc0df05e9b5b0d3d0ad5c9cfe3
Mike <misha354@gmail.com>
2015-02-11 22:33:43 -0500

    implement

--- GuessingGameGUI/GuessingGameGUI/GuessingGameForm.Designer.cs
+++ GuessingGameGUI/GuessingGameGUI/GuessingGameForm.Designer.cs
@@ -28,51 +28,139 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.label1 = new System.Windows.Forms.Label();
+         this.lblQuestion = new System.Windows.Forms.Label();
          this.btnNewGame = new System.Windows.Forms.Button();
          this.btnExit = new System.Windows.Forms.Button();
+         this.lblInstruction = new System.Windows.Forms.Label();
+         this.rtxHistory = new System.Windows.Forms.RichTextBox();
+         this.lblNumGuessesTitle = new System.Windows.Forms.Label();
+         this.lblNumGuesses = new System.Windows.Forms.Label();
+         this.label1 = new System.Windows.Forms.Label();
+         this.txtGuess = new System.Windows.Forms.TextBox();
+         this.lblSecret = new System.Windows.Forms.Label();
+         this.lblMessage = new System.Windows.Forms.Label();
          this.SuspendLayout();
          // 
-         // label1
+         // lblQuestion
          // 
-         this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(12, 9);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(169, 26);
-         this.label1.TabIndex = 0;
-         this.label1.Text = "I have a number between 1 and\r\n1000. Can you guess the number?";
-         this.label1.Click += new System.EventHandler(this.label1_Click);
+         this.lblQuestion.AutoSize = true;
+         this.lblQuestion.Location = new System.Drawing.Point(22, 12);
+         this.lblQuestion.Name = "lblQuestion";
+         this.lblQuestion.Size = new System.Drawing.Size(169, 26);
+         this.lblQuestion.TabIndex = 0;
+         this.lblQuestion.Text = "I have a number between 1 and\r\n1000. Can you guess the number?";
          // 
          // btnNewGame
          // 
-         this.btnNewGame.Location = new System.Drawing.Point(197, 12);
+         this.btnNewGame.Location = new System.Drawing.Point(237, 12);
          this.btnNewGame.Name = "btnNewGame";
          this.btnNewGame.Size = new System.Drawing.Size(75, 23);
          this.btnNewGame.TabIndex = 1;
-         this.btnNewGame.Text = "button1";
+         this.btnNewGame.Text = "&New Game";
          this.btnNewGame.UseVisualStyleBackColor = true;
-         this.btnNewGame.Click += new System.EventHandler(this.button1_Click);
+         this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
          // 
          // btnExit
          // 
-         this.btnExit.Location = new System.Drawing.Point(197, 41);
+         this.btnExit.Location = new System.Drawing.Point(237, 41);
          this.btnExit.Name = "btnExit";
          this.btnExit.Size = new System.Drawing.Size(75, 23);
          this.btnExit.TabIndex = 2;
          this.btnExit.Text = "E&xit";
          this.btnExit.UseVisualStyleBackColor = true;
+         this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+         // 
+         // lblInstruction
+         // 
+         this.lblInstruction.AutoSize = true;
+         this.lblInstruction.Location = new System.Drawing.Point(12, 73);
+         this.lblInstruction.Name = "lblInstruction";
+         this.lblInstruction.Size = new System.Drawing.Size(77, 13);
+         this.lblInstruction.TabIndex = 3;
+         this.lblInstruction.Text = "Enter a Guess:";
+         // 
+         // rtxHistory
+         // 
+         this.rtxHistory.Location = new System.Drawing.Point(15, 103);
+         this.rtxHistory.Name = "rtxHistory";
+         this.rtxHistory.Size = new System.Drawing.Size(216, 124);
+         this.rtxHistory.TabIndex = 5;
+         this.rtxHistory.Text = "";
+         // 
+         // lblNumGuessesTitle
+         // 
+         this.lblNumGuessesTitle.AutoSize = true;
+         this.lblNumGuessesTitle.Location = new System.Drawing.Point(12, 239);
+         this.lblNumGuessesTitle.Name = "lblNumGuessesTitle";
+         this.lblNumGuessesTitle.Size = new System.Drawing.Size(103, 13);
+         this.lblNumGuessesTitle.TabIndex = 7;
+         this.lblNumGuessesTitle.Text = "Number of Guesses:";
+         // 
+         // lblNumGuesses
+         // 
+         this.lblNumGuesses.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+         this.lblNumGuesses.Location = new System.Drawing.Point(121, 236);
+         this.lblNumGuesses.Name = "lblNumGuesses";
+         this.lblNumGuesses.Size = new System.Drawing.Size(29, 20);
+         this.lblNumGuesses.TabIndex = 8;
+         this.lblNumGuesses.Text = "0";
+         this.lblNumGuesses.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+         // 
+         // label1
+         // 
+         this.label1.Location = new System.Drawing.Point(12, 268);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(40, 13);
+         this.label1.TabIndex = 9;
+         // 
+         // txtGuess
+         // 
+         this.txtGuess.Location = new System.Drawing.Point(91, 69);
+         this.txtGuess.Name = "txtGuess";
+         this.txtGuess.Size = new System.Drawing.Size(37, 20);
+         this.txtGuess.TabIndex = 10;
+         // 
+         // lblSecret
+         // 
+         this.lblSecret.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+         this.lblSecret.Location = new System.Drawing.Point(194, 69);
+         this.lblSecret.Name = "lblSecret";
+         this.lblSecret.Size = new System.Drawing.Size(37, 20);
+         this.lblSecret.TabIndex = 4;
+         this.lblSecret.Text = "0";
+         this.lblSecret.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+         // 
+         // lblMessage
+         // 
+         this.lblMessage.AutoSize = true;
+         this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.lblMessage.Location = new System.Drawing.Point(12, 268);
+         this.lblMessage.Name = "lblMessage";
+         this.lblMessage.Size = new System.Drawing.Size(41, 13);
+         this.lblMessage.TabIndex = 11;
+         this.lblMessage.Text = "label2";
          // 
          // GuessingGameForm
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(284, 262);
+         this.ClientSize = new System.Drawing.Size(324, 299);
+         this.Controls.Add(this.lblMessage);
+         this.Controls.Add(this.txtGuess);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.lblNumGuesses);
+         this.Controls.Add(this.lblNumGuessesTitle);
+         this.Controls.Add(this.rtxHistory);
+         this.Controls.Add(this.lblSecret);
+         this.Controls.Add(this.lblInstruction);
          this.Controls.Add(this.btnExit);
          this.Controls.Add(this.btnNewGame);
-         this.Controls.Add(this.label1);
+         this.Controls.Add(this.lblQuestion);
          this.KeyPreview = true;
          this.Name = "GuessingGameForm";
-         this.Text = "Form1";
+         this.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+         this.Text = "Guessing Game";
+         this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GuessingGameForm_KeyDown);
          this.ResumeLayout(false);
          this.PerformLayout();
 
@@ -80,9 +168,17 @@
 
       #endregion
 
-      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label lblQuestion;
       private System.Windows.Forms.Button btnNewGame;
       private System.Windows.Forms.Button btnExit;
+      private System.Windows.Forms.Label lblInstruction;
+      private System.Windows.Forms.RichTextBox rtxHistory;
+      private System.Windows.Forms.Label lblNumGuessesTitle;
+      private System.Windows.Forms.Label lblNumGuesses;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox txtGuess;
+      private System.Windows.Forms.Label lblSecret;
+      private System.Windows.Forms.Label lblMessage;
    }
 }
 
--- GuessingGameGUI/GuessingGameGUI/GuessingGameForm.cs
+++ GuessingGameGUI/GuessingGameGUI/GuessingGameForm.cs
@@ -7,24 +7,196 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace GuessingGameGUI
 {
    public partial class GuessingGameForm : Form
    {
+
+      Game g; //the guessing game object.
+
       public GuessingGameForm()
       {
          InitializeComponent();
+
+         //use the game's boundaries to pose question to user
+         lblQuestion.Text = String.Format("I have a number between {0} and\n" +
+                               "{1}. Can you guess the number?", Game.LOW, Game.HI);
+         InitGame();  //create a new game object and update the GUI state
+         this.KeyPreview = true;
       }
 
-      private void button1_Click(object sender, EventArgs e)
+
+      private void btnExit_Click(object sender, EventArgs e)
       {
-
+         this.Close();
       }
 
-      private void label1_Click(object sender, EventArgs e)
+      private void btnNewGame_Click(object sender, EventArgs e)
       {
-
+         InitGame();
       }
+
+      //event handler for an enter keypress
+      private void GuessingGameForm_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Enter)
+         {
+            e.SuppressKeyPress = true;
+            AcceptGuess();
+         }
+      }
+
+      //non-event handler private methods
+
+      //reads the users guess from the textbox and drives execution
+      //forward
+      private void AcceptGuess()
+      {
+         //try to play the user's guess
+         g.TryGuess(txtGuess.Text);
+
+         //Do tasks that get done after both correct or incorrect guesses
+         DoAfterGuessTasks();
+
+         if (g.Guessed)
+         //Do tasks tasks that get done only when the user guessed correctly
+         {
+            DoAfterCorrectGuessTasks();
+         }
+
+         else
+         //Do tasks tasks that get done only when the user guessed wrong
+         {
+            DoAfterWrongGuessTasks();
+         }
+      }
+
+
+
+      //Resets form to state it should have
+      //after the user has guessed correctly
+      private void DoAfterCorrectGuessTasks()
+      {
+         txtGuess.Enabled = false;
+      }
+
+      //Tasks that should be done after guess,
+      //regardless of whether it's correct
+      private void DoAfterGuessTasks()
+      {
+         UpdateHistory(); //update the history text box
+         UpdateNumTries();
+
+         SetBackColor();
+
+         //update the number of tries textbox
+         lblNumGuesses.Text = g.NumTries.ToString();
+
+         //Update the status message label
+         lblMessage.Text = g.Status;
+
+         //enable the new game button
+         btnNewGame.Enabled = true;
+
+         //clear the guess textbox
+         txtGuess.Clear();
+      }
+
+      //sets the controls to states they should
+      //after an incorrect guess
+      private void DoAfterWrongGuessTasks()
+      {
+         btnNewGame.Enabled = true;
+         SetBackColor();
+         lblNumGuesses.Text = g.NumTries.ToString();
+      }
+
+      //creates a new game object and sets gui state to new game
+      private void InitGame()
+      {
+         g = new Game(); //get a new game object
+
+         //set the form color to default
+         this.BackColor = SystemColors.Control;
+
+         //enable controls that make sense for this game state
+         btnNewGame.Enabled = false;
+         txtGuess.Enabled = true; //enable the guess textbox
+
+         //Gather information from the game and write it to the GUI
+         lblMessage.Text = g.Status;
+         lblSecret.Text = g.SecretNum.ToString(); //for debugging
+         UpdateNumTries();
+
+         txtGuess.Clear(); //clear content from last games
+         rtxHistory.Clear();
+      }
+
+      // sets the background color after reading a guess
+      private void SetBackColor()
+      {
+         Debug.WriteLine("entered backcolor");
+         //if user guessed correctly, make background light green
+         if (g.Guessed) this.BackColor = Color.LightGreen;
+
+         //user guessed wrong
+         else
+         {
+            Debug.WriteLine("back color, processing wrong guess");
+            //if this isn't the first guess and if this guess is different from the last one
+            Debug.WriteLine("right before the if statement that makes sure not first guess and not same guess as last");
+
+            if (g.NumTries > 1 && !g.GuessRepeat)
+            {
+               if (g.GuessCloser)
+               //user got closer, so make screen red for "warmer"
+               {
+                  Debug.WriteLine("setbackcolor: setting salmon");
+                  this.BackColor = Color.LightSalmon;
+               }
+               //user is further away, so make screen blue for "cold"
+               else
+               {
+                  Debug.WriteLine("setbackcolor: setting green");
+                  this.BackColor = Color.LightBlue;
+               }
+            }
+         }
+      }
+
+      //updates the history textbox based on the result
+      //of an interaction with the user
+      private void UpdateHistory()
+      {
+         //If the user guessed the number, the game is over, so no need to update the history box
+         if (!g.Guessed)
+         {
+            if (!g.LastGuessValid)
+            //user entered invalid input, print an error message
+            {
+               rtxHistory.Text += String.Format("{0}: Invalid input\n", txtGuess.Text);
+            }
+
+            else if (g.GuessHi)
+            //user guessed too low
+            {
+               rtxHistory.Text += String.Format("{0} - Too high, guess a bit lower\n", g.UsersGuess);
+
+            }
+            else
+            //the user guessed too low
+            {
+               rtxHistory.Text += String.Format("{0} - Too low, guess a bit higher\n", g.UsersGuess);
+            }
+         }
+      }
+
+      private void UpdateNumTries()
+      {
+         lblNumGuesses.Text = (g.NumTries + 1).ToString();
+      }
+
    }
 }
